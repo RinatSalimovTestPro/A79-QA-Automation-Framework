6921181cde74ccd82b011675c7f1c2cadf02d7b9
RinatSalimovTestPro <rinat.salimov@testpro.io>
2026-02-27 14:09:55 -0500

    Homework23

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,4 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+/*import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -44,4 +44,58 @@ public class BaseTest  {
     public void navigatingToPage() {
         driver.get(url);
     }
+}*/
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.time.Duration;
+
+public class BaseTest  {
+
+    public static WebDriver driver = null;
+    public String url = null;
+    public static WebDriverWait wait = null;
+    public static Actions actions = null;
+
+    @BeforeMethod
+    @Parameters({"baseUrl"})
+    public void launchBrowser(@Optional("https://qa.koel.app/") String baseUrl) throws MalformedURLException {
+
+        // options for Chrome on Grid
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        // Selenium Grid URL (standalone)
+        String gridUrl = "http://172.26.253.121:4444";
+        // sometimes also works: "http://localhost:4444"
+
+        driver = new RemoteWebDriver(java.net.URI.create(gridUrl).toURL(), options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        url = baseUrl;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+
+        navigatingToPage();
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public void navigatingToPage() {
+        driver.get(url);
+    }
 }
+
